dc80a2dff8a337d658dd45b6cadab783383d0307
cczallen <cczallen@gmail.com>
2014-08-16 17:22:17 +0800

    + Add many functions
    + Add demo code

--- ALUtilities/ViewController.m
+++ ALUtilities/ViewController.m
@@ -10,6 +10,7 @@
 #import "ALUtilities.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -23,7 +24,32 @@
 	repeat(5, ^(size_t i) {
 		NSLog(@"LOG:  i: %li",i);
 	});
-//	[NSString stringWithFormat:@"%@", ];
+
+	NSLog(@"//---------------------------");
+	
+	dispatchAfter(1, ^{
+		NSMutableDictionary *dic = [@{} mutableCopy];
+		repeat(1000, ^(size_t i) {
+			int r = GetRandomBetween(0, 9);
+			NSLog(@"LOG:  r: %i",r);
+			if (dic[@(r)]) {
+				NSInteger count = [dic[@(r)] integerValue] +1;
+				dic[@(r)] = @(count);
+			}else {
+				dic[@(r)] = @(1);
+			}
+		});
+		
+		NSMutableString *str = [@"GetRandomBetween(0, 9):\n {\n" mutableCopy];
+		for (id key in [dic sortedKeys]) {
+			id value = dic[key];
+			[str appendFormat:@"  %@: %@\n", key, value];
+		}
+		[str appendFormat:@"  }\n"];
+		
+		NSLog(@"LOG:  %@",str);
+		self.textView.text = str;
+	});
 }
 
 - (void)didReceiveMemoryWarning
